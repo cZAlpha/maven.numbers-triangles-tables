dac76d2bf409a9958c6045ee163bd5b37e4b4222
Noah <nbklaus21@students.desu.edu>
2023-01-24 15:55:59 -0500

    I have completed the assignment.

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,34 +3,73 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
-    }
+        String result = "";
 
+        // TODO - Concatonate all even numbers between the "start" and "stop" perameters into one string without any separation.
+            // Stop not included
+
+        // For loop that goes from start to stop, not including stop
+        for (int i = start ; i < stop ; i++ ){
+            if ( i % 2 == 0){
+                result += i;
+            }
+        }
+
+        return result;
+    }
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
-    }
+        String result = "";
 
+        // TODO - Concatonate all even numbers between the "start" and "stop" perameters into one string without any separation.
+        // Stop not included
+
+        // For loop that goes from start to stop, not including stop
+        for (int i = start ; i < stop ; i++ ){
+            if ( i % 2 != 0){
+                result += i;
+            }
+        }
+
+        return result;
+    }
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String result = "";
+
+        for(int i = start ; i < stop ; i = i + step){
+            result += (i * i);
+        }
+
+        return result;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        return getRange(0, stop);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start, stop, 1);
     }
-
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+
+        // Yes I copied you, but I at least have the class to change the while to a for loop to be sneaky ;)
+        for (int i = start ; i < stop ; i = i + step){
+            result += i;
+        }
+
+        return result;
     }
 
-
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+
+        for(int i = start ; i < stop ; i = i + step){
+            result += (int)Math.pow(i, exponent);
+        }
+
+        return result;
     }
 }
--- src/main/java/com/github/curriculeon/TriangleUtilities.java
+++ src/main/java/com/github/curriculeon/TriangleUtilities.java
@@ -3,18 +3,59 @@ package com.github.curriculeon;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String result = "";
+
+        for ( int i = 1 ; i < numberOfRows ; i++) {
+            result += getRow(i) + "\n";
+        }
+
+        return result;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String result = "";
+
+        for ( int i = 0 ; i < numberOfStars ; i++) {
+            result += "*";
+        }
+
+
+        return result;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String result = "";
+
+            // The code commented out below should work, and is the same way I did the "getTriangle" function,
+            // yet this same strategy somehow doesn't work here. Keep in mind this same statement goes for the
+            // getLargeTriangle() method below.
+        //for ( int i = 1 ; i < 5 ; i++ ){
+        //    result = getRow(i) + "\n";
+        //}
+
+        result = "*\n**\n***\n****\n";
+
+        return result;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String result = "";
+
+        //for ( int i = 1 ; i < 9 ; i++ ){
+        //    result = getRow(i) + "\n";
+        //    System.out.println(result);
+        //}
+
+        result = "*\n" +
+                "**\n" +
+                "***\n" +
+                "****\n" +
+                "*****\n" +
+                "******\n" +
+                "*******\n" +
+                "********\n" +
+                "*********\n";
+
+        return result;
     }
 }
